60908fad3a5cf0abc0ea6da89b70a84f2dc24328
DANIEL <daniel.tabares1@udea.edu.co>
2021-01-26 22:48:58 -0500

    Guardado y busqueda de archivos al momento de registrar

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+
 public class LoginController {
+
+    private JFXTextField txtId;
+    private JFXTextField txtContra;
+
+
+
+    public LoginController(){
+
+    }
 }
+
+
---
+++ src/dao/impl/LoginDAOArchivo.java
@@ -0,0 +1,6 @@
+package dao.impl;
+
+import dao.LoginDAO;
+
+public class LoginDAOArchivo implements LoginDAO {
+}
