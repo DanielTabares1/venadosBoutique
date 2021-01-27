ae5c4637912e9ee3ff34b26947aab9c17eb9de15
DANIEL <daniel.tabares1@udea.edu.co>
2021-01-27 00:04:25 -0500

    modificacion en la pestaña login

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,16 +1,55 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import dao.impl.LoginDAOArchivo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 public class LoginController {
 
+    @FXML
     private JFXTextField txtId;
     private JFXTextField txtContra;
 
-
+    private LoginDAOArchivo loginDAOArchivo;
 
     public LoginController(){
+        this.loginDAOArchivo = new LoginDAOArchivo();
+    }
+    public void verificarUsuario(){
+        String nombreIngresado = txtId.getText().trim();
+        String contra = txtContra.getText().trim();
 
+        //validaciones para contra
+        if(contra.isEmpty()){
+            mostrarMensaje(Alert.AlertType.ERROR, "Inicio de sesion", "Resultado de la transacción", "Contraseña es requerida");
+            txtContra.requestFocus();
+            return;
+        }
+        //validaciones para nombres
+        if(nombreIngresado.isEmpty()){
+            mostrarMensaje(Alert.AlertType.ERROR, "Inicio de seison", "Resultado de la transacción", "El nombre es requerido");
+            txtId.requestFocus();
+            return;
+        }
+
+        boolean usuarioExiste = loginDAOArchivo.verificarUsuario(nombreIngresado,contra);
+
+        if(usuarioExiste){
+            mostrarMensaje(Alert.AlertType.INFORMATION, "Inicio de seison", "Resultado de la transacción", "Inicio de sesion exitoso");
+        }
+        else if(!usuarioExiste){
+            mostrarMensaje(Alert.AlertType.ERROR, "Inicio de sesion", "Resultado de la transacción", "Usuario no registrado.");
+        }
+
+    }
+
+    private void mostrarMensaje(Alert.AlertType tipo, String titulo, String encabezado, String mensaje) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(encabezado);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 }
 
--- src/dao/LoginDAO.java
+++ src/dao/LoginDAO.java
@@ -1,4 +1,6 @@
 package dao;
 
 public interface LoginDAO {
+
+    boolean verificarUsuario(String nombre, String contra);
 }
